1ff0e759ba10e8356efcee5831d7d2a70cec49c8
PAPICO <papico@inflearn.com>
2020-09-28 22:00:22 +0900

    feat: 액션 함수 정의

--- src/index.js
+++ src/index.js
@@ -7,3 +7,13 @@ const $counter = document.querySelector('.counter-heading');
 const $plus = document.querySelector('#plus-btn');
 const $minus = document.querySelector('#minus-btn');
 
+// 엑션 타입 정의
+const TOGGLE_SWITCH = 'TOGGLE_SWITCH';
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+
+// 액션 생성 함수 정의
+const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
+const increment = diff => ({ type: INCREMENT, diff });
+const decrement = () => ({ type: DECREMENT });
+
