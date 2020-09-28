d6991a2a0f9f61cc45f248173be3053e115774cc
PAPICO <papico@inflearn.com>
2020-09-28 21:57:49 +0900

    feat: init 🚀

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { createStore } from 'redux';
+
+const $light = document.querySelector('.light');
+const $switch = document.querySelector('#switch-btn');
+
+const $counter = document.querySelector('.counter-heading');
+const $plus = document.querySelector('#plus-btn');
+const $minus = document.querySelector('#minus-btn');
+
